1f1cd817b6ff2e029f1d9eedd77ececdbb738d31
Daniel Furman <djfurman@gmail.com>
2021-06-24 19:10:59 -0400

    Add test for undefined number of terms. Import stdlib random libaray. Use random.randint to generate a random number of terms between two and twelve. Loop over the number of terms to test and use random.randint again to generate a random integer between 1 and 1000 and append it to the terms list as a string. Initalize a checksum to zero and then add teach term before casting it to a string. Assert that calculator returns an integer. Assert that what calculator returns matches the checksum.

--- string calculator/test_calculate.py
+++ string calculator/test_calculate.py
@@ -1,4 +1,7 @@
+import random
+
 from calculate import calculate
+
 
 def test_calculate():
     assert True
@@ -8,3 +11,18 @@ def test_it_takes_two_comma_seperated_numbers_and_returns_a_sum():
     result = calculate(user_input)
 
     assert result == 3
+
+def test_it_handles_n_amount_of_numbers():
+    terms_we_will_test = random.randint(2, 12)
+
+    terms = []
+    checksum = 0
+    for _ in range(terms_we_will_test):
+        term = random.randint(1, 1000)
+        checksum = checksum + term
+        terms.append(str(term))
+
+    result = calculate(",".join(terms))
+
+    assert isinstance(result, int)
+    assert checksum == result
