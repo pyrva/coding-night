c69d5f5c827e8a1060baa423d67ec183bf355805
Daniel Furman <djfurman@gmail.com>
2021-06-24 19:23:11 -0400

    Add code to fix test three. Handle the split with re so we can use an arbitrary number of terms.

--- string calculator/calculate.py
+++ string calculator/calculate.py
@@ -1,4 +1,10 @@
+import re
+
 def calculate(data: str) -> float:
-    inputs = data.split(",")
+    # Splits on comma OR newline into an interable
+    inputs = re.split(",|\n", data)
+    
+    # List interpolation to cast each item as int
     cast_inputs = [int(i) for i in inputs]
-    return sum(cast_inputs)
+    
+    return sum(cast_inputs)  # sum everything in the list and return
